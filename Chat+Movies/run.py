ee2e1c47e1eadcdd48efbdc923bc87559c58869b
Johhnnnnyy202020 <ionutgigellaiu@gmail.com>
2020-09-08 18:55:32 +0000

    Trying to display movies

--- Chat+Movies/run.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
-
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient('mongodb+srv://Netflix:Netflix@cluster0.7n1lg.mongodb.net/sample_mflix?retryWrites=true&w=majority')
-db = client.sample_mflix
-
-
-print(db.list_collection_names())
---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'sample_mflix'
+app.config["MONGO_URI"] = "mongodb+srv://Netflix:Netflix@cluster0.7n1lg.mongodb.net/sample_mflix?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("movies.html", tasks=mongo.db.movies.find())
